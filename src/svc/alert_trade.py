0970f538696f34e2bb95a928628a1bf17bb14ac4
Kyle Doan <khoadoan1991@gmail.com>
2024-02-21 13:06:48 -0800

    fix regex parsing

--- src/svc/alert_trade.py
+++ src/svc/alert_trade.py
@@ -9,23 +9,24 @@ limit1Matcher = re.compile(r'[Ll]imit\s*1[:]?\s*(?P<limit1>[0-9.,]+)')
 limit2Matcher = re.compile(r'[Ll]imit\s*2[:]?\s*(?P<limit1>[0-9.,]+)')
 
 def request_map_to_alert(request: TradingViewRequestBody) -> TradingViewAlert:
+  now = getSettingCurrentTime()
   if "ENTER" in request.orderComment:
     # match stoploss
     stopMatch = stopMatcher.search(request.orderComment)
-    stop = float(stopMatch.group(1)) if stopMatch is not None else None
+    stop = float(stopMatch.group(1).replace(",", "")) if stopMatch is not None else None
     
     # match limit1
     limit1Match = limit1Matcher.search(request.orderComment)
-    limit1 = float(limit1Match.group(1)) if limit1Match is not None else None
+    limit1 = float(limit1Match.group(1).replace(",", "")) if limit1Match is not None else None
     
     # match limit2
     limit2Match = limit2Matcher.search(request.orderComment)
-    limit2 = float(limit2Match.group(1)) if limit2Match is not None else None
+    limit2 = float(limit2Match.group(1).replace(",", "")) if limit2Match is not None else None
     
     trend = "ERROR" if limit2 is None or limit1 is None else "UP" if limit2 > limit1 else "DOWN"
     
     return TradingViewAlert(
-      received_at=getSettingCurrentTime(),
+      received_at=now,
       ticker=request.ticker,
       action="ENTER",
       trend=trend,
@@ -37,7 +38,7 @@ def request_map_to_alert(request: TradingViewRequestBody) -> TradingViewAlert:
     )
   
   return TradingViewAlert(
-    received_at=getSettingCurrentTime(),
+    received_at=now,
     ticker=request.ticker,
     action="EXIT",
     quantity=request.positionSize,
